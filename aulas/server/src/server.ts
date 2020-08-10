e7e3672afd5e2e1b8bc981ae0addd3f5ae616af1
Michael Douglas do Amarante <doug.amarante@gmail.com>
2020-08-10 01:10:56 -0300

    update web

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -9,12 +9,13 @@
 
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 
 //http://localhost:3333
 app.listen(3333);
